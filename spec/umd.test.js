355cab8d0fae7e1da7418e6c725c422867540b85
G.Ambatte <sean@robertson-family.nz>
2026-05-27 21:25:05 +1200

    Eliminate unneeded files for JS package

--- spec/umd.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, test } from 'node:test';
-import '../lib/index.umd.js';
-
-describe('marked-extension-template umd', () => {
-  test('test umd global', (t) => {
-    t.assert.equal(typeof markedExtensionTemplate, 'function');
-  });
-});
